00508062b78519472492a7c16a3e6ec21deaa1ad
felixbor <felixbor@gmail.com>
2022-11-30 23:20:35 -0400

    2

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(".search").on("click", function (event) {
     $(".result").empty();
 
     fetchDrink()
-      
+
 
 
     function fetchDrink() {
@@ -20,35 +20,67 @@ $(".search").on("click", function (event) {
             })
             .then(function (data) {
                 console.log(data);
-             displayDrinks(data)})
+                displayDrinks(data)
+            })
     }
 })
 
 function displayDrinks(data) {
-    var cocklic1  = data.drinks[0].strDrink
-    var cocklic2  = data.drinks[1].strDrink
-    var cocklic3  = data.drinks[2].strDrink
-    var cocklic4  = data.drinks[3].strDrink
-    var cocklic5  = data.drinks[4].strDrink
-    //var { icon } = data.weather[0]
-    //var { description } = data.weather[0];
-   // var { temp } = data.main
-    //var { humidity } = data.main
-    //var { speed } = data.wind
+    var cocklic1 = data.drinks[0].strDrink
+    var cocklic2 = data.drinks[1].strDrink
+    var cocklic3 = data.drinks[2].strDrink
+    var cocklic4 = data.drinks[3].strDrink
+    var cocklic5 = data.drinks[4].strDrink
+    var cocklic6 = data.drinks[5].strDrink
+    var cocklic7 = data.drinks[6].strDrink
+    var cocklic8 = data.drinks[7].strDrink
+    var cocklic9 = data.drinks[8].strDrink
+    var cocklic10 = data.drinks[9].strDrink
+
     console.log(cocklic1)
     console.log(cocklic2)
     console.log(cocklic3)
     console.log(cocklic4)
     console.log(cocklic5)
-    $(".result").append(`<div>${cocklic1}</div>`);
-    }
+    $(".result").append(`<button>${cocklic1}</button>`);
+    $(".result").append(`<button>${cocklic2}</button>`);
+    $(".result").append(`<button>${cocklic3}</button>`);
+    $(".result").append(`<button>${cocklic4}</button>`);
+    $(".result").append(`<button>${cocklic5}</button>`);
+    $(".result").append(`<button>${cocklic6}</button>`);
+    $(".result").append(`<button>${cocklic7}</button>`);
+    $(".result").append(`<button>${cocklic8}</button>`);
+    $(".result").append(`<button>${cocklic9}</button>`);
+    $(".result").append(`<button>${cocklic10}</button>`);
+}
+$(".result").on("click", selectDrink)
+function selectDrink(event) {
+    var name = event.target.textContent
+    fetchRecipe(name)
 
-   /* document.getElementById("city").innerText = name;
-    document.getElementById("temperature").innerText = temp + "°C";
-    document.getElementById("humidity").innerText = humidity + "%"
-    document.getElementById("wind").innerText = speed + "km/h";
-    document.getElementById("condition").innerText = description;
-    document.querySelector("#icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+}
+
+function fetchRecipe(name) {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+   
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+        })
+
+}
+
+
+/* document.getElementById("city").innerText = name;
+ document.getElementById("temperature").innerText = temp + "°C";
+ document.getElementById("humidity").innerText = humidity + "%"
+ document.getElementById("wind").innerText = speed + "km/h";
+ document.getElementById("condition").innerText = description;
+ document.querySelector("#icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
 
 */
 /*let codes = {
